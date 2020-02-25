72477eed4d4add211fa05cea704db4887c5658f9
Michal Hazucha <hazuchamichal@gmail.com>
2020-02-25 18:02:10 +0100

    Navbar added

---
+++ symfony/src/Controller/ArticleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkExtraBundle\Controller\Controller;
+
+
+class ArticleController extends AbstractController
+{
+	/**
+	 * @TODO
+	 * jednoduchý kontroler a template- hardcoded data ako by to malo vyzerať
+	 */
+
+	/**
+	 * @Route("/")
+	 * @Method({"GET"})
+	 */
+	public function index(){
+		return $this->render('articles/index.html.twig');
+	}
+};
+
